1e685338d7c960309d4f6b336738859499417265
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-05-14 00:00:14 +0300

    fix: import error

--- backend/src/CCIPTokenSender.sol
+++ backend/src/CCIPTokenSender.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.19;
 import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
-import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/token/ERC20/IERC20.sol";
-import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {IERC20} from "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 
 contract CCIPTokenSender is OwnerIsCreator {
     IRouterClient router;
