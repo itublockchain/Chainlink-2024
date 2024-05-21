a65f573e98df9b5326b402d49e3da76675f8507a
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-05-21 22:01:29 +0300

    calculation error fixed

--- backend/src/EthToUsdc.sol
+++ backend/src/EthToUsdc.sol
@@ -6,9 +6,6 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./Oracle.sol";
 
-
-
-
 contract EthToUsdcConverter {
     Oracle public oracle;
     IERC20 public usdcToken;
@@ -23,7 +20,7 @@ contract EthToUsdcConverter {
         require(msg.value > 0, "Deposit amount must be greater than zero");
         int ethUsdPrice = oracle.getThePrice();
         require(ethUsdPrice > 0, "Invalid price from oracle");
-        uint256 usdAmount = (msg.value * uint256(ethUsdPrice)) / 10**8;
+        uint256 usdAmount = (msg.value * uint256(ethUsdPrice)) / 10 ** 20;
         userDeposits[msg.sender] += usdAmount;
     }
 
@@ -32,12 +29,12 @@ contract EthToUsdcConverter {
         require(usdAmount > 0, "No USD amount to withdraw");
 
         userDeposits[msg.sender] = 0;
-        bool success = usdcToken.transfer(msg.sender, (usdAmount/1e10) * 10**6);
+        bool success = usdcToken.transfer(msg.sender, usdAmount);
         require(success, "USDC transfer failed");
     }
 
     function withdrawForNoIsraf(uint256 value) public {
-         bool success = usdcToken.transfer(msg.sender, value * 10**6);
+        bool success = usdcToken.transfer(msg.sender, value * 10 ** 6);
         require(success, "USDC transfer failed");
     }
 }
