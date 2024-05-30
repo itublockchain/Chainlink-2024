854fa6c9d76b4827c28dca552bf7321235f9775e
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-05-30 21:59:47 +0300

    Sender Contract Update

---
+++ backend/src/CCTP/Avalanche/AvalancheSender.sol
@@ -0,0 +1,262 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {IRouterClient} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {OwnerIsCreator} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {Client} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {Staker} from "./Staker.sol";
+
+// ROUTER 0xf694e193200268f9a4868e4aa017a0118c9a8177
+// LINK 0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846
+// USDC 0x5425890298aed601595a70AB815c96711a31Bc65
+// Avax/USD Avalanche Data Feed 0x5498BB86BC934c8D34FDA08E81D444153d0D06aD
+// SEPOLIA Destination 16015286601757825753
+// 0.027 = 27000000000000000
+
+    interface IPangolinRouter {
+    function swapExactAVAXForTokens(
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable returns (uint[] memory amounts);
+}
+/// @title - A simple messenger contract for transferring tokens to a receiver  that calls a staker contract.
+contract AvalancheSender is OwnerIsCreator {
+    using SafeERC20 for IERC20;
+
+
+
+    // Custom errors to provide more descriptive revert messages.
+    error InvalidRouter(); // Used when the router address is 0
+    error InvalidLinkToken(); // Used when the link token address is 0
+    error InvalidUsdcToken(); // Used when the usdc token address is 0
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees); // Used to make sure contract has enough balance to cover the fees.
+    error NothingToWithdraw(); // Used when trying to withdraw Ether but there's nothing to withdraw.
+    error InvalidDestinationChain(); // Used when the destination chain selector is 0.
+    error InvalidReceiverAddress(); // Used when the receiver address is 0.
+    error NoReceiverOnDestinationChain(uint64 destinationChainSelector); // Used when the receiver address is 0 for a given destination chain.
+    error AmountIsZero(); // Used if the amount to transfer is 0.
+    error InvalidGasLimit(); // Used if the gas limit is 0.
+    error NoGasLimitOnDestinationChain(uint64 destinationChainSelector); // Used when the gas limit is 0.
+
+    // Event emitted when a message is sent to another chain.
+    event MessageSent(
+        bytes32 indexed messageId, // The unique ID of the CCIP message.
+        uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
+        address indexed receiver, // The address of the receiver contract on the destination chain.
+        address beneficiary, // The beneficiary of the staked tokens on the destination chain.
+        address token, // The token address that was transferred.
+        uint256 tokenAmount, // The token amount that was transferred.
+        address feeToken, // the token address used to pay CCIP fees.
+        uint256 fees // The fees paid for sending the message.
+    );
+
+    IRouterClient private immutable i_router;
+    IERC20 private immutable i_linkToken;
+    IERC20 private immutable i_usdcToken;
+    AggregatorV3Interface private oracle;
+    IPangolinRouter public router = IPangolinRouter(0x2D99ABD9008Dc933ff5c0CD271B88309593aB921);
+    address public WAVAX = 0xd00ae08403B9bbb9124bB305C09058E32C39A48c;
+
+    // Mapping to keep track of the receiver contract per destination chain.
+    mapping(uint64 => address) public s_receivers;
+    // Mapping to store the gas limit per destination chain.
+    mapping(uint64 => uint256) public s_gasLimits;
+    mapping(address => uint256) public userDeposits;
+
+    // Magic Numbers
+    //int public avaxUsdPrice = 3736436355;
+
+    modifier validateDestinationChain(uint64 _destinationChainSelector) {
+        if (_destinationChainSelector == 0) revert InvalidDestinationChain();
+        _;
+    }
+
+    /// @notice Constructor initializes the contract with the router address.
+    /// @param _router The address of the router contract.
+    /// @param _link The address of the link contract.
+    /// @param _usdcToken The address of the usdc contract.
+    constructor(address _router, address _link, address _usdcToken) {
+        if (_router == address(0)) revert InvalidRouter();
+        if (_link == address(0)) revert InvalidLinkToken();
+        if (_usdcToken == address(0)) revert InvalidUsdcToken();
+        i_router = IRouterClient(_router);
+        i_linkToken = IERC20(_link);
+        i_usdcToken = IERC20(_usdcToken);
+        oracle = AggregatorV3Interface(0x5498BB86BC934c8D34FDA08E81D444153d0D06aD);
+    }
+
+    /// @dev Set the receiver contract for a given destination chain.
+    /// @notice This function can only be called by the owner.
+    /// @param _destinationChainSelector The selector of the destination chain.
+    /// @param _receiver The receiver contract on the destination chain .
+    function setReceiverForDestinationChain(
+        uint64 _destinationChainSelector,
+        address _receiver
+    ) external onlyOwner validateDestinationChain(_destinationChainSelector) {
+        if (_receiver == address(0)) revert InvalidReceiverAddress();
+        s_receivers[_destinationChainSelector] = _receiver;
+    }
+
+    /// @dev Set the gas limit for a given destination chain.
+    /// @notice This function can only be called by the owner.
+    /// @param _destinationChainSelector The selector of the destination chain.
+    /// @param _gasLimit The gas limit on the destination chain .
+    function setGasLimitForDestinationChain(
+        uint64 _destinationChainSelector,
+        uint256 _gasLimit
+    ) external onlyOwner validateDestinationChain(_destinationChainSelector) {
+        if (_gasLimit == 0) revert InvalidGasLimit();
+        s_gasLimits[_destinationChainSelector] = _gasLimit;
+    }
+
+    /// @dev Delete the receiver contract for a given destination chain.
+    /// @notice This function can only be called by the owner.
+    /// @param _destinationChainSelector The selector of the destination chain.
+    function deleteReceiverForDestinationChain(
+        uint64 _destinationChainSelector
+    ) external onlyOwner validateDestinationChain(_destinationChainSelector) {
+        if (s_receivers[_destinationChainSelector] == address(0))
+            revert NoReceiverOnDestinationChain(_destinationChainSelector);
+        delete s_receivers[_destinationChainSelector];
+    }
+
+    /// @notice Sends data and transfer tokens to receiver on the destination chain.
+    /// @notice Pay for fees in LINK.
+    /// @dev Assumes your contract has sufficient LINK to pay for CCIP fees.
+    /// @param _destinationChainSelector The identifier (aka selector) for the destination blockchain.
+    /// @param _beneficiary The address of the beneficiary of the staked tokens on the destination blockchain.
+    /// @param _amount token amount.
+    /// @return messageId The ID of the CCIP message that was sent.
+    function sendMessagePayLINK(
+        uint64 _destinationChainSelector,
+        address _beneficiary,
+        uint256 _amount
+    )
+        external
+        onlyOwner
+        validateDestinationChain(_destinationChainSelector)
+        returns (bytes32 messageId)
+    {
+        address receiver = s_receivers[_destinationChainSelector];
+        if (receiver == address(0))
+            revert NoReceiverOnDestinationChain(_destinationChainSelector);
+        if (_amount == 0) revert AmountIsZero();
+        uint256 gasLimit = s_gasLimits[_destinationChainSelector];
+        if (gasLimit == 0)
+            revert NoGasLimitOnDestinationChain(_destinationChainSelector);
+        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
+        // address(linkToken) means fees are paid in LINK
+        Client.EVMTokenAmount[]
+            memory tokenAmounts = new Client.EVMTokenAmount[](1);
+        tokenAmounts[0] = Client.EVMTokenAmount({
+            token: address(i_usdcToken),
+            amount: _amount
+        });
+        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
+        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver), // ABI-encoded receiver address
+            data: abi.encodeWithSelector(
+            Staker.stake.selector,
+                _beneficiary,
+                _amount,
+                msg.sender
+            ), // Encode the function selector and the arguments of the stake function
+            tokenAmounts: tokenAmounts, // The amount and type of token being transferred
+            extraArgs: Client._argsToBytes(
+                // Additional arguments, setting gas limit
+                Client.EVMExtraArgsV1({gasLimit: gasLimit})
+            ),
+            // Set the feeToken to a feeTokenAddress, indicating specific asset will be used for fees
+            feeToken: address(i_linkToken)
+        });
+
+        // Get the fee required to send the CCIP message
+        uint256 fees = i_router.getFee(
+            _destinationChainSelector,
+            evm2AnyMessage
+        );
+
+        if (fees > i_linkToken.balanceOf(address(this)))
+            revert NotEnoughBalance(i_linkToken.balanceOf(address(this)), fees);
+
+        // approve the Router to transfer LINK tokens on contract's behalf. It will spend the fees in LINK
+        i_linkToken.approve(address(i_router), fees);
+
+        // approve the Router to spend usdc tokens on contract's behalf. It will spend the amount of the given token
+        i_usdcToken.approve(address(i_router), _amount);
+
+        // Send the message through the router and store the returned message ID
+        messageId = i_router.ccipSend(
+            _destinationChainSelector,
+            evm2AnyMessage
+        );
+        
+        userDeposits[msg.sender] = userDeposits[msg.sender] - _amount;
+        // Emit an event with message details
+        emit MessageSent(
+            messageId,
+            _destinationChainSelector,
+            receiver,
+            _beneficiary,
+            address(i_usdcToken),
+            _amount,
+            address(i_linkToken),
+            fees
+        );
+
+        // Return the message ID
+        return messageId;
+    }
+
+    function deposit() public payable {
+        require(msg.value > 0, "Deposit amount must be greater than zero");
+        (,int avaxUsdPrice,,,) = oracle.latestRoundData();
+        require(avaxUsdPrice > 0, "Invalid price from oracle");
+        address[] memory path = new address[](2);
+        path[0] = WAVAX;        
+        path[1] = address(i_usdcToken);   
+        uint deadline = block.timestamp + 900; 
+        uint256 usdAmount = (msg.value * uint256(avaxUsdPrice)) / 10 ** 20;
+         router.swapExactAVAXForTokens{ value: msg.value }(
+            usdAmount,
+            path,
+            address(this),
+            deadline
+        );
+        userDeposits[msg.sender] += usdAmount;
+
+    }
+
+     
+
+    /// @notice Allows the owner of the contract to withdraw all LINK tokens in the contract and transfer them to a beneficiary.
+    /// @dev This function reverts with a 'NothingToWithdraw' error if there are no tokens to withdraw.
+    /// @param _beneficiary The address to which the tokens will be sent.
+    function withdrawLinkToken(address _beneficiary) public onlyOwner {
+        // Retrieve the balance of this contract
+        uint256 amount = i_linkToken.balanceOf(address(this));
+
+        // Revert if there is nothing to withdraw
+        if (amount == 0) revert NothingToWithdraw();
+
+        i_linkToken.safeTransfer(_beneficiary, amount);
+    }
+
+    /// @notice Allows the owner of the contract to withdraw all usdc tokens in the contract and transfer them to a beneficiary.
+    /// @dev This function reverts with a 'NothingToWithdraw' error if there are no tokens to withdraw.
+    /// @param _beneficiary The address to which the tokens will be sent.
+    function withdrawUsdcToken(address _beneficiary) public onlyOwner {
+        // Retrieve the balance of this contract
+        uint256 amount = i_usdcToken.balanceOf(address(this));
+
+        // Revert if there is nothing to withdraw
+        if (amount == 0) revert NothingToWithdraw();
+
+        i_usdcToken.safeTransfer(_beneficiary, amount);
+    }
+}
