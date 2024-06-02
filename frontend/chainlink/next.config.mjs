7b7c55fa2ed6348ddae59a2f7d403c1213c2224e
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-06-02 15:58:47 +0300

    fix

--- frontend/chainlink/next.config.mjs
+++ frontend/chainlink/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        ignoreBuildErrors: true,
-      },
+  typescript: {
+    reactStrictMode: true,
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
